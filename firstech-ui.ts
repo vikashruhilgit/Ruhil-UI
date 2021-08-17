91b27f10a77215c987aeb9849fada394fdca4e9c
Apple <apple@Apples-MacBook-Pro.local>
2021-08-17 15:17:17 +0530

    add prefix global

--- firstech-ui.ts
+++ firstech-ui.ts
@@ -1,11 +1,13 @@
+import { prefix } from './config.js';
+
 import { App } from './src/component/App.js';
 import { Test } from './src/component/Test.js';
 import { GooglePay } from './src/component/gpay/GooglePay.js';
 import { PlayGround } from './src/Play-ground.js';
 import { Loader } from './src/component/loader/Loader.js';
 
-window.customElements.define('ft-test', Test);
-window.customElements.define('ft-app', App);
-window.customElements.define('ft-google-pay', GooglePay);
-window.customElements.define('ft-play-ground', PlayGround);
-window.customElements.define('ft-loader', Loader);
+window.customElements.define(`${prefix}-test`, Test);
+window.customElements.define(`${prefix}-app`, App);
+window.customElements.define(`${prefix}-google-pay`, GooglePay);
+window.customElements.define(`${prefix}-play-ground`, PlayGround);
+window.customElements.define(`${prefix}-loader`, Loader);
