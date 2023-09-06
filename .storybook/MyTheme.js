1367ec073cc43a06e2ad75d3135f363ce519771b
vikash <vikash.ruhil@busey.com>
2023-09-06 14:47:16 +0530

    chore: misc

--- .storybook/MyTheme.js
+++ .storybook/MyTheme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-  brandTitle: 'Ruhil UI Library',
+  brandTitle: 'Ruhil UI',
   brandUrl: 'https://www.linkedin.com/in/vikash-ruhil/',
   colorSecondary: 'deepskyblue',
 });
